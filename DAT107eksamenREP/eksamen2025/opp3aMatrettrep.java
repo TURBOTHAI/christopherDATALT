7c5acd8dac37cfe428970eec03bf8a833e4aa927
798118 <164620581+TURBOTHAI@users.noreply.github.com>
2025-04-29 20:50:39 +0200

    sd
    sd

---
+++ DAT107eksamenREP/eksamen2025/opp3aMatrettrep.java
@@ -0,0 +1,5 @@
+package eksamen2025;
+
+public class opp3aMatrettrep {
+
+}
---
+++ DAT107eksamenREP/opp3aMatrett.java
@@ -0,0 +1,56 @@
+
+package eksamen2025;
+
+import java.math.BigDecimal;
+
+public class opp3aMatrett {
+	
+	@Entity//Entity er det samme som en relasjon kan du si "tabell"
+	
+	public class Matrett{//må ha en klasse på selve relasjonen
+		@Id GeneratedValue (strategy= GenerationType.IDENTETY)
+		private int id;// her må id være under for at den skal bli godkjent som GeneratedValue
+		
+		private String navn;
+		private String kategori;
+		private BigDecimal salgspris;
+		
+		@ManyToMany(fetch = FetchType.EAGER)// denne henter data fra matrett-tabellen og data fra ingrediens-tabellen
+		//det ManyToMany med fetch gjør er at den forklarer Hibernate at denne varibelen( som er en liste= representerer en mange-til-mange-relajson
+		@JoinTable(
+			name = "exam_har_ingrediens",
+			joinColums = @JoinColumn(name="matrett_id"),
+			inversJoinColumns= @JoinColumn(name="ingreduens_id"))
+		private List<Ingrediens> ingredienser;
+		//endret
+	}
+	
+}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+	
+		
+		
+	}
+
+}
