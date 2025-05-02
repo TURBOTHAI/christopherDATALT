c477fef697a42e6003cb2ff9b815ee9e88b8463c
798118 <164620581+TURBOTHAI@users.noreply.github.com>
2025-05-02 22:58:24 +0200

    js

--- DAT107eksamenREP/eksamen2024/opp3a.java
+++ DAT107eksamenREP/eksamen2024/opp3a.java
@@ -1,5 +1,50 @@
 package eksamen2024;
 
 public class opp3a {
+	
+	
+	@Entity
+	public class Bok {
+		@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+		private int id;
+		private String tittel;
+		private String forfatter;
+		private String hylle;
+		
+		
+		
+		
+	}
+	
+	@Entity
+	public class Medlem {
+		@Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+		private int nr;
+		private String fornavn;
+		private String etternavn;
+		private String telefon;
+		
+		@ManyToOne (mappedby="Medlem", fetch = FetchType.EAGER)
+		private List<Utlaan> utlaan;
+		
+	}
+	
+	@Entity
+	public class Utlaan{
+		@Id @GeneratedValue(stategy = GenerationType.IDENTITY)
+		private int id;
+		
+		@ManyToOne @JoinColumn(name="bokid")
+		private Bok bokid;
+		
+		@ManyToOne @JoinColumn(name="medlemnr")
+		private Medlem medlemnr;
+		
+		private String utlaandato;
+		private String forfalldato;
+		private String returdato;
+		
+		
+	}
 
 }
